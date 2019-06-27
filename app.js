7852d9495528e2a40514d48d74e1880d9b1929d7
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-27 01:17:19 -0400

    Set the loosing conditions.

--- app.js
+++ app.js
@@ -30,7 +30,14 @@ guessBtn.addEventListener('click', function(){
         guessInput.style.borderColor = 'green';
         setMessage(`${winningNumber} is correct! YOU WIN!`, 'green');
     } else {
-        
+        guessesLeft -= 1;
+
+        if (guessesLeft === 0) {
+            // Game over
+            guessInput.disabled = true;
+            guessInput.style.borderColor = 'red';
+            setMessage(`Game over you lost! The correct number was ${winningNumber}`, 'red', '1px ssolid red');
+        }
     }
 });
 
