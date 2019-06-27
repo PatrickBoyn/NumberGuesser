16f94136b59136b276f1dfde3083cf7cbdde4dc5
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-27 15:21:13 -0400

    Changed it to a return statement.

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function gameOver(won, color) {
 }
 
 function getWinningNumber(min, max){
-
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function setMessage(msg, color='black', bgcolor='none', border='none') {
     message.style.color = color;
