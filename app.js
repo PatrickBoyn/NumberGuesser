e2776859f16f6596f79e37944d65ddb7d9bf70f1
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-27 14:44:56 -0400

    Changed it so that guessing something outside the bounds does not use a  guess.

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ guessBtn.addEventListener('click', function(){
 
     if (isNaN(guess) || guess < min || guess > max) {
         setMessage(`Please enter a number between ${min} and ${max}`, '#D8000C', '#FFBABA', '1px solid #D8000C');
-    }
-
-    if (guess === winningNumber) {
+    } else if (guess === winningNumber) {
         gameOver(true, 'green');
         setMessage(`${winningNumber} is correct! YOU WIN!`, 'green');
     } else {
